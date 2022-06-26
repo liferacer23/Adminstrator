89962ee941080f78b4eb0659484750aa9f756135
liferacer23 <binyamg23@gmail.com>
2022-06-26 14:02:01 +0300

    chart addded

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,7 +1,65 @@
-import React from 'react'
-
+import React  from 'react';
+import { Area, AreaChart, XAxis,  CartesianGrid, Tooltip,  ResponsiveContainer } from 'recharts';
+const data = [
+  {
+ name:"January",
+ Total:3220
+  },
+  {
+ name:"February",
+ Total:1000
+  },
+  {
+ name:"March",
+ Total:2354
+  },
+  {
+ name:"April",
+ Total:3211
+  },
+  {
+ name:"May",
+ Total:800
+  },
+  {
+ name:"June",
+ Total:902
+  },
+  
+  {
+ name:"July",
+ Total:3420
+  },
+  {
+ name:"August",
+ Total:2420
+  },
+  
+];
 export default function Chart() {
-  return ( 
-    <div className='shadow-3xl w-[60%] p-2'>Chart</div>
-  )
+ 
+    return (
+      <div className='shadow-3xl w-[60%] p-2 text-sm'>
+        <h1>Last 6 Months (Revenue)</h1>
+      <ResponsiveContainer width="100%" aspect={2/1}>
+      <AreaChart width={730} height={250} data={data}
+  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  <defs>
+    <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
+  <XAxis dataKey="name" stroke='gray'/>
+{/* <YAxis /> */}
+  <CartesianGrid strokeDasharray="3 3" />
+  <Tooltip />
+  <Area type="monotone" dataKey="Total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />
+</AreaChart>
+    </ResponsiveContainer>
+  </div>
+    );
+  
+ 
+  
 }
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -2,6 +2,8 @@ import React from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowDownUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 export default function Featured() {
   return (
     <div className="shadow-3xl flex flex-col p-2 w-[35%]">
@@ -12,11 +14,29 @@ export default function Featured() {
       <div className="flex flex-col space-y-4 items-center">
         <div className="w-[100px] h-[100px]"> 
           <CircularProgressbar value={70} text="70%" strokeWidth={4} />
-        </div>
+        </div>  
         <div className="flex flex-col items-center space-y-2 ">
           <h1 className="text-sm font-semibold text-gray-500">Total sales made today</h1>
           <h1 className="text-3xl font-semibold">$400</h1>
           <h1 className="text-xs text-center text-gray-500 w-[70%]">Previous transaction processing payment may not be included</h1>
+         {/* Summary */}
+         <div className="flex space-x-5 text-sm">
+          <div className="flex flex-col items-center space-y-3">
+            <div className="text-gray-800"> Target</div>
+            <div className="flex items-center text-red-500" ><KeyboardArrowDownIcon className="text-red-500"/>  $12.6k</div>
+        
+          </div>
+          <div className="flex flex-col items-center space-y-3">
+            <div className="text-gray-800"> Last Weekend</div>
+            <div className="flex items-center text-green-500" ><KeyboardArrowDownUpOutlinedIcon className="text-green-500" />  $12.6k</div>
+        
+          </div>
+          <div className="flex flex-col items-center space-y-3">
+            <div className="text-gray-800"> Last Month</div>
+            <div className="flex items-center text-green-500" ><KeyboardArrowDownUpOutlinedIcon className="text-green-500" />  $12.6k</div>
+        
+          </div>
+          </div>
         </div>
       </div>
     </div>
